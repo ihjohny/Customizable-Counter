9d14c987e140c0d056fabda67ccce9f2815fd5ad
Marvin Mouroum <Mouroum.m@gmail.com>
2023-12-11 12:14:25 +0100

    Added Prefix and Suffix options

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -15,16 +15,22 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // The button will be shown first
+    expect(find.text('Add Item'), findsOneWidget);
+
+    // Tapp the button to start the counter
+    await tester.tap(find.text('Add Item'));
+    await tester.pump();
+
+    // Verify that our counter starts at 1 (because of the Add Item).
+    expect(find.text('1%'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1%'), findsNothing);
+    expect(find.text('2%'), findsOneWidget);
   });
 }
